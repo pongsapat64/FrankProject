f93a14a5df3ec69404a576e3aba7b99ba35d60e5
User <User@DESKTOP-ACOGSIM>
2022-09-25 12:55:40 +0700

    lab5

---
+++ src/java/model/CarInfo.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author HP
+ */
+public class CarInfo {
+    private String brand;
+    private String model;
+    private int horse_power;
+    private int price;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getHorse_power() {
+        return horse_power;
+    }
+
+    public void setHorse_power(int horse_power) {
+        this.horse_power = horse_power;
+    }
+}
